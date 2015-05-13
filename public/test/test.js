5105422c974b5ed50e6109c3542aec295a613f60
userkiller <a7505489@hotmail.com>
2015-05-13 12:49:52 -0400

    added qunit testing

---
+++ public/test/test.js
@@ -0,0 +1,34 @@
+var c3 = require [('/public/lib/c3.min')]
+
+test("check for c3", c3, function(c3){
+expect (1);
+ok(c3, c3 file is in the current directory)
+})
+
+var d3 = require [('/public/lib/d3.min')]
+
+test("check for d3", d3, function(d3){
+expect (1);
+ok(d3, d3 file is in the current directory)
+})
+
+var topten = require [('/public/lib/topten.min')]
+
+test("check for topten", topten, function(topten){
+expect (1);
+ok(topten, topten file is in the current directory)
+})
+
+var visuals = require [('/public/lib/visuals.min')]
+
+test("check for visuals", visuals, function(visuals){
+expect (1);
+ok(visuals, visuals file is in the current directory)
+})
+
+var Stylesheet = require [('/public/stylesheets/style.css')]
+
+test("check for Stylesheet", Stylesheet, function(Stylesheet){
+expect (1);
+ok(Stylesheet, Stylesheet file is in the current directory)
+})
